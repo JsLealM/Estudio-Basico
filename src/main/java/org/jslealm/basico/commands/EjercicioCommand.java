34c3fed342465452089b035fc5048ff46846fa55
JsLealM <johanleal2002@gmail.com>
2026-08-09 00:14:41 -0500

    Ejercicios Completados

--- src/main/java/org/jslealm/basico/commands/EjercicioCommand.java
+++ src/main/java/org/jslealm/basico/commands/EjercicioCommand.java
@@ -9,16 +9,22 @@ import org.bukkit.entity.Player;
 public class EjercicioCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player player && args.length == 0){
+        if (sender instanceof Player player){
+            if (args.length != 0){
+                if (args[0].equalsIgnoreCase("admin") && player.hasPermission("ejercicio.admin")){
+                    adminCommand(player, args);
+                    return true;
+                }
+                subCommands(sender, args);
+                return true;
+            }
             player.sendMessage(String.format("Hola, %s", player.getName()));
-        }
-        if (args.length != 0){
-            subCommands(sender, args);
+            return true;
         }
         return true;
     }
 
-    private boolean subCommands (CommandSender sender, String[] args){
+    private void subCommands (CommandSender sender, String[] args){
         if (args.length == 1){
             String arg = args[0].toLowerCase();
             String commands = """
@@ -28,7 +34,15 @@ public class EjercicioCommand implements CommandExecutor {
                 """;
             switch (arg){
                 case "hola" -> sender.sendMessage("Has Utilizado el Argumento hola");
-                case "coins" -> sender.sendMessage("Has utilizado el Argumento coins");
+                case "coins" -> {
+                    if (sender instanceof Player){
+                        sender.sendMessage("Has utilizado el Argumento coins");
+                        return;
+                    }else {
+                        sender.sendMessage("El comando solo puede ser usado por jugadores");
+                        return;
+                    }
+                }
                 case "help" -> sender.sendMessage(commands);
                 case "lives" -> sender.sendMessage("Has utilizado el Argumento Lives");
                 case "gift" -> {
@@ -39,7 +53,7 @@ public class EjercicioCommand implements CommandExecutor {
                 }
                 default -> sender.sendMessage("Utiliza el argumento /help para ver los comandos disponibles");
         }
-        return true;
+        return;
         }
         if (!(sender instanceof Player) && args[0].equalsIgnoreCase("gift") && args.length == 4){
             if ((args[1].equalsIgnoreCase("coins") ||args[1].equalsIgnoreCase("lives")) && !(args[2].isEmpty()) && !(args[3].isEmpty())) {
@@ -48,7 +62,7 @@ public class EjercicioCommand implements CommandExecutor {
                     Player player = Bukkit.getPlayer(args[2]);
                     if (player == null){
                         sender.sendMessage("El jugador no existe o no esta conectado");
-                        return true;
+                        return;
                     }
                     sender.sendMessage(String.format("""
                             Sistema: gift
@@ -62,10 +76,14 @@ public class EjercicioCommand implements CommandExecutor {
             }else
                 sender.sendMessage("El argumento se usa de esta forma /ej gift <coins or lives> <player> <cantidad>");
         }else
-            sender.sendMessage("El argumento se usa de esta forma /ej gift <coins or lives> <player> <cantidad>");
-        return true;
+            sender.sendMessage("Utiliza el argumento /help para ver los comandos disponibles");
     }
 
-
-
+    private void adminCommand (Player sender, String[] args){
+        if (args.length != 2 || !(args[1].equalsIgnoreCase("reload"))){
+            sender.sendMessage("Has utilizado el comando admin. Para recargar haz /monda admin reload");
+            return;
+        }
+        sender.sendMessage("Configuracion recargada correctamente");
+    }
 }
